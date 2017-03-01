a7405b6d23c7ab3fb699e6dace6c4f89d9d4b751
Ryan Racioppo <ryanracioppo@gmail.com>
2017-03-01 16:13:39 -0500

    GO

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
   get 'example_mines/test1' => "example_mines#view_data"
   get 'example_mines/shapefileTest' => "example_mines#shapefileTest"
 end
